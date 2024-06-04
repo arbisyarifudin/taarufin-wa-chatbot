cbb04b86690149d1d617d2b574dd8d6257cd9a4b
Arbi Syarifudin <arbisyarifudin@gmail.com>
2024-06-04 10:18:22 +0700

    add pm2 > ecosystem.config.js file

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'taarufin-chatbot-wa',
+      script: 'bot.js',
+    },
+    {
+      name: 'taarufin-chatbot-api',
+      script: 'api.js',
+    }
+  ],
+};
