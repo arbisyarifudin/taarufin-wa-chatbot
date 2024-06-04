dfb48501948777fcade141e4fdd7b712a40b7ae8
Arbi Syarifudin <arbisyarifudin@gmail.com>
2024-06-04 10:17:52 +0700

    chore: update port number in api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { sequelize, Setting, Block, BlockOption } = require('./models');
 
 const app = express();
-const port = 3000;
+const port = 1000;
 
 app.use(bodyParser.json());
 
